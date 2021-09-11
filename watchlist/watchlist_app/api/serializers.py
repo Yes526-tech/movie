777ad2966301693db65a7785c4057a9b8943051e
salih <salihsert18@gmail.com>
2021-09-11 20:50:04 +0200

    create new project setup based on previous movie api

--- watchlist/watchlist_app/api/serializers.py
+++ watchlist/watchlist_app/api/serializers.py
@@ -1,80 +1,16 @@
 from rest_framework import serializers
-from watchlist_app.models import Movie
+from watchlist_app.models import WatchList, StreamPlatform
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class StreamPlatformSerializer(serializers.ModelSerializer):
 
-    # Below we defined a method with get and field name. custom name
-    len_name = serializers.SerializerMethodField()
-    # default serializers need update and create mthods
-    # but the model serialziers has its own methods
     class Meta:
-        model = Movie
+        model = StreamPlatform
         fields = "__all__"
-        
-        # if we dont want active field. we dont need to pass fields if
-        # we define exclude
-        # exclude = ['active']
-
-    # we need to define validate methods if we need them manually
-    # field level validation
-    def validate_name(self, value):
-        if len(value) < 4:
-            raise serializers.ValidationError("Name is too short!")
-        else:
-            return value
-
-    # Object level validation
-    def validate(self, data):
-        if data['name'] == data['description']:
-            raise serializers.ValidationError(
-                "Name and Description should be different!")
-        else:
-            return data
-
-    def get_len_name(self, object):
-        return len(object.name)
 
 
-# Validate by validators
+class WatchListSerializer(serializers.ModelSerializer):
 
-
-# def name_length(value):
-#     if len(value) < 4:
-#         raise serializers.ValidationError("Name is too short!")
-#     else:
-#         return value
-
-
-# class MovieSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(validators=[name_length])
-#     description = serializers.CharField()
-#     active = serializers.BooleanField()
-
-#     def create(self, validated_data):
-#         return Movie.objects.create(**validated_data)
-
-#     def update(self, instance, validated_data):
-#         # instance carries old values, validated_data is the new values
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.description = validated_data.get(
-#             'description', instance.description)
-#         instance.active = validated_data.get('active', instance.active)
-#         instance.save()
-#         return instance
-
-    # field level validation
-    # def validate_name(self, value):
-    #     if len(value) < 4:
-    #         raise serializers.ValidationError("Name is too short!")
-    #     else:
-    #         return value
-
-    # Object level validation
-    # def validate(self, data):
-    #     if data['name'] == data['description']:
-    #         raise serializers.ValidationError(
-    #             "Name and Description should be different!")
-    #     else:
-    #         return data
+    class Meta:
+        model = WatchList
+        fields = "__all__"
--- watchlist/watchlist_app/api/urls.py
+++ watchlist/watchlist_app/api/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 # from watchlist_app.api.views import movie_list, movie_details
-from watchlist_app.api.views import MovieListAV, MovieDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV
 
-
-# urlpatterns = [
-#     path('list/', movie_list, name='movie_list'),
-#     path('<int:pk>/', movie_details, name='movie_detail')
-# ]
 
 urlpatterns = [
-    path('list/', MovieListAV.as_view(), name='movie_list'),
-    path('<int:pk>/', MovieDetailAV.as_view(), name='movie_detail')
+    path('list/', WatchListAV.as_view(), name='watch_list'),
+    path('<int:pk>/', WatchDetailAV.as_view(), name='watch_detail'),
+    path('stream/', StreamPlatformAV.as_view(), name='streamplatform')
 ]
     
