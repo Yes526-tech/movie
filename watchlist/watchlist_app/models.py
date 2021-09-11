d01fad81d2e11e17b804b52a188fab050790841d
salih <salihsert18@gmail.com>
2021-09-11 14:41:16 +0200

    Add serializers.Serializer to view

--- watchlist/watchlist_app/models.py
+++ watchlist/watchlist_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    activate = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
     
     def __str__(self):
         return self.name
