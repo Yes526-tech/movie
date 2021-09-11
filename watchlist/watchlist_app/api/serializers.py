b3d2bbec150fabffc9b153193f92248596c4acc1
salih <salihsert18@gmail.com>
2021-09-11 17:50:39 +0200

    add different type of validators

--- watchlist/watchlist_app/api/serializers.py
+++ watchlist/watchlist_app/api/serializers.py
@@ -1,20 +1,45 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+# Validate by validators
+
+
+def name_length(value):
+    if len(value) < 4:
+        raise serializers.ValidationError("Name is too short!")
+    else:
+        return value
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
     def create(self, validated_data):
-       return Movie.objects.create(**validated_data)
+        return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-       # instance carries old values, validated_data is the new values 
-       instance.name = validated_data.get('name', instance.name)
-       instance.description = validated_data.get('description', instance.description)
-       instance.active = validated_data.get('active', instance.active)
-       instance.save()
-       return instance
+        # instance carries old values, validated_data is the new values
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+
+    # field level validation
+    # def validate_name(self, value):
+    #     if len(value) < 4:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     else:
+    #         return value
+
+    # Object level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(
+                "Name and Description should be different!")
+        else:
+            return data
