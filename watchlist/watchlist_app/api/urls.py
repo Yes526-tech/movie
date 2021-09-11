66b81cea1bc36e495bd3b1ae12e4379633baf2ff
salih <salihsert18@gmail.com>
2021-09-11 16:59:15 +0200

    API Views implemented

--- watchlist/watchlist_app/api/urls.py
+++ watchlist/watchlist_app/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from watchlist_app.api.views import movie_list, movie_details
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
+
+# urlpatterns = [
+#     path('list/', movie_list, name='movie_list'),
+#     path('<int:pk>/', movie_details, name='movie_detail')
+# ]
 
 urlpatterns = [
-    path('list/', movie_list, name='movie_list'),
-    path('<int:pk>/', movie_details, name='movie_detail')
+    path('list/', MovieListAV.as_view(), name='movie_list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie_detail')
 ]
-  
+    
