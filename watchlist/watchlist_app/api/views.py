164a93ea42f91a45dc2664aab166d0437c627cf3
salih <salihsert18@gmail.com>
2021-09-11 15:27:03 +0200

    Add put patch delete methods

--- watchlist/watchlist_app/api/views.py
+++ watchlist/watchlist_app/api/views.py
@@ -3,16 +3,43 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view(['GET'])
-def movie_list(request):
-    movies = Movie.objects.all()
-    # Serializer should be run many times. 
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        # Serializer should be run many times.
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+# we cannot make post request because we have already access item itself
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)  
-   
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        # To update an item we have to pass the movie object into the serializers
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        # we dont need serializers to delete
+        movie.delete()
+        return Response()
